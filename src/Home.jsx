44c16f9ca3497b5a6e7ef05ecbb6bb6580727941
Leirad Noznag <leiradcompany@gmail.com>
2026-04-29 12:41:43 +0800

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -19,6 +19,17 @@ export default function Home() {
                 if(res.success) setData(res);
             })
             .catch(() => {});
+
+        // Load Tawk.to Chat Widget
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/69f18b77db9e841c36b065d3/1jnbohiu5';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
     }, []);
 
     const handleSubscribe = async (e) => {
