b625982fefe9f7aac41f5359ae0022e1df855ff9
Leirad Noznag <leiradcompany@gmail.com>
2026-05-18 12:29:04 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/*" element={<App />} />
         <Route path="/admin/*" element={<Admin />} />
         <Route path="/cookie-policy" element={<CookiePolicy />} />
       </Routes>
